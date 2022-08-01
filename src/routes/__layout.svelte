c103ca3463bb3ac0bc269070a1bcdc4dfe720cd7
jpark6 <qkstjr09@gmail.com>
2022-08-01 17:53:14 +0900

    Feat: face transition

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,12 +1,27 @@
 <!-- Layout Component-->
+<script context="module">
+const currentRoute = url.pathname;
+
+return {
+  props: {
+    currentRoute
+  }
+};
+</script>
 <script lang="ts">
 import Header from "./lib/components/Header.svelte";
 import './lib/styles/style.scss';
+import { fade } from 'svelte/transition';
 </script>
 
 <Header />
-<main>
+{#key currentRoute}
+<main
+  in:fade={{ duration: 150, delay: 150}}
+  out:fade{{ duration: 150}}
+>
   <slot />
 </main>
+{/key}
 
 <footer>This is a Footer~</footer>
