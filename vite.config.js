676c4f4da2b22e9d67244470fe2debbfcdf6c9b5
jpark6 <qkstjr09@gmail.com>
2023-08-05 06:53:40 +0900

    Feat: add alias in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import path from 'path';
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	alias: {
+		$components: path.resolve('./src/components'),
+		$assets: path.resolve('./src/assets'),
 	}
 };
 
